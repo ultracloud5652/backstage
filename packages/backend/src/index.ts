8cd5bbebea937b1943f9fed00ee3770310e0de63
Kolawole Oduremi <kolawole.oduremi@gmail.com>
2024-10-16 18:29:56 +0100

    Update index.ts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -17,6 +17,8 @@ backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
